60f4987c0449e9b0c9a12443f6bb6243190125a2
Austin-Draper <31936666+Austin-Draper@users.noreply.github.com>
2018-12-12 15:57:57 -0600

    Add files via upload

---
+++ build-Encryption-Desktop_Qt_5_11_2_MinGW_32bit-Release/ui_widget.h
@@ -0,0 +1,74 @@
+/********************************************************************************
+** Form generated from reading UI file 'widget.ui'
+**
+** Created by: Qt User Interface Compiler version 5.11.2
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_WIDGET_H
+#define UI_WIDGET_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QHBoxLayout>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_Widget
+{
+public:
+    QHBoxLayout *horizontalLayout_2;
+    QHBoxLayout *horizontalLayout;
+    QPushButton *encryptButton;
+    QPushButton *decryptButton;
+
+    void setupUi(QWidget *Widget)
+    {
+        if (Widget->objectName().isEmpty())
+            Widget->setObjectName(QStringLiteral("Widget"));
+        Widget->resize(400, 300);
+        horizontalLayout_2 = new QHBoxLayout(Widget);
+        horizontalLayout_2->setSpacing(6);
+        horizontalLayout_2->setContentsMargins(11, 11, 11, 11);
+        horizontalLayout_2->setObjectName(QStringLiteral("horizontalLayout_2"));
+        horizontalLayout = new QHBoxLayout();
+        horizontalLayout->setSpacing(6);
+        horizontalLayout->setObjectName(QStringLiteral("horizontalLayout"));
+        encryptButton = new QPushButton(Widget);
+        encryptButton->setObjectName(QStringLiteral("encryptButton"));
+
+        horizontalLayout->addWidget(encryptButton);
+
+        decryptButton = new QPushButton(Widget);
+        decryptButton->setObjectName(QStringLiteral("decryptButton"));
+
+        horizontalLayout->addWidget(decryptButton);
+
+
+        horizontalLayout_2->addLayout(horizontalLayout);
+
+
+        retranslateUi(Widget);
+
+        QMetaObject::connectSlotsByName(Widget);
+    } // setupUi
+
+    void retranslateUi(QWidget *Widget)
+    {
+        Widget->setWindowTitle(QApplication::translate("Widget", "Widget", nullptr));
+        encryptButton->setText(QApplication::translate("Widget", "Encrypt", nullptr));
+        decryptButton->setText(QApplication::translate("Widget", "Decrypt", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class Widget: public Ui_Widget {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_WIDGET_H
